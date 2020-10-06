5faf9243cd68dc8e6c52f6f32690387b7b8fe438
CHIN JING JIE <kocekwong@gmail.com>
2020-10-06 22:56:35 +0800

    change console size, add food and obstacles with random position

---
+++ SnakeGame/Food.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    public class Food
+    {
+        private string _food;
+        private int _foodx;
+        private int _foody;
+
+        public Food() { }
+
+        public Food(string food, int foodx, int foody) {
+            _food = food;
+            _foodx = foodx;
+            _foody = foody;
+        }
+
+        public string SnakeFood
+        {
+            get { return _food; }
+            set { _food = value; }
+        }
+
+        public int FoodX
+        {
+            get { return _foodx; }
+            set { _foodx = value; }
+        }
+
+        public int FoodY
+        {
+            get { return _foody; }
+            set { _foody = value; }
+        }
+
+        public void GenerateFood()
+        {
+            Console.SetCursorPosition(_foodx, _foody);
+            Console.Write(_food);
+        }
+    }
+}
---
+++ SnakeGame/Obstacles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    public class Obstacles
+    {
+        private string _obstacle;
+        private int _obstaclex;
+        private int _obstacley;
+
+        public Obstacles() { }
+
+        public Obstacles(string obstacle, int obstaclex, int obstacley)
+        {
+            _obstacle = obstacle;
+            _obstaclex = obstaclex;
+            _obstacley = obstacley;
+        }
+
+        public string SnakeObstacles
+        {
+            get { return _obstacle; }
+            set { _obstacle = value; }
+        }
+
+        public int ObstacleX
+        {
+            get { return _obstaclex; }
+            set { _obstaclex = value; }
+        }
+
+        public int ObstacleY
+        {
+            get { return _obstacley; }
+            set { _obstacley = value; }
+        }
+
+        public void GenerateObstacles()
+        {
+            Console.SetCursorPosition(_obstaclex, _obstacley);
+            Console.Write(_obstacle);
+        }
+    }
+}
